d9af921055cd0c2520b3d386a51b27a5e14d07df
Tarini Chandrashekhar <tarini.chandrashekhar@gmail.com>
2017-07-06 19:19:52 +0530

    Delete mser.m

--- MATAB Codes/Final Working Codes/mser.m
+++
@@ -1,7 +0,0 @@
-I = imread('test1.png');
-regions= detectMSERFeatures(I);
-figure; imshow(I); hold on;
-plot(regions, 'showPixelList', true, 'showEllipses', false);
-figure; imshow(I);
-hold on;
-plot(regions);
