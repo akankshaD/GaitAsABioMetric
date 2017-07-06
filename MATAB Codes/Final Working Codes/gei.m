7862940f84e9c3cdd621bf923d7b887636cdf38c
Tarini Chandrashekhar <tarini.chandrashekhar@gmail.com>
2017-07-06 19:17:39 +0530

    Delete gei.m

--- MATAB Codes/Final Working Codes/gei.m
+++
@@ -1,8 +0,0 @@
-I0 = imread('test1.png')
-sumImage = double(I0); % Inialize to first image.
-for i=2:10 % Read in remaining images.
-  rgbImage = imread(['test',num2str(i),'.png']);
-  sumImage = sumImage + double(rgbImage);
-end;
-meanImage = sumImage / 10;
-imshow(meanImage);
